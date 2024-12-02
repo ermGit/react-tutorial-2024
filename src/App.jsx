5e851faf16fe3446a4fe0041b569be1edce50ab6
FSHAC\emoore1 <emoore1@fshac.com>
2024-12-02 16:39:45 -0600

    4hrs-into-video: Add new Users.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import {useState} from "react";
 
 export default function App() {
     const isAuthenticated = true;
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [counter, setCounter] = useState(5);
 
     const [mockUsers, setUsers] = useState([
         {
@@ -31,41 +34,86 @@ export default function App() {
     ]);
 
     return isAuthenticated ? (
-            <div>
-                {mockUsers.map(
-                    (user) => {
-                        return (
-                            <UserDetails
-                                key={user.id}
-                                setUsers={setUsers}
-                                user={user}
-                            />
-                        );
-                    }
-                )}
+        <div>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    console.log(username);
+                    console.log(email);
+                    const newUser = {
+                        id: counter,
+                        username: username,
+                        email: email,
+                    };
+                    setCounter(
+                        (currentCounter) => currentCounter + 1
+                    );
+                    setUsers(
+                        (currentUsersState) => [...currentUsersState, newUser]
+                    );
+                }}
+            >
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <br/>
+                    <input
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <br/>
+                    <input
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </div>
+                <button type="submit">Add User</button>
+            </form>
+            <br/>
+            {mockUsers.map(
+                (user) => {
+                    return (
+                        <UserDetails
+                            key={user.id}
+                            setUsers={setUsers}
+                            user={user}
+                        />
+                    );
+                }
+            )}
 
-                <h1>Root Component</h1>
-                <UserProfile
-                    username="Bob"
-                    age={25}
-                    favoriteFoods={[
-                        {
-                            name: "Sushi",
-                            id: "sushi",
-                        },
-                        {
-                            name: "Pizza",
-                            id: "pizza",
-                        }
-                    ]}
-                />
-            </div>
+            <h1>Root Component</h1>
+            <UserProfile
+                username="Bob"
+                age={25}
+                favoriteFoods={[
+                    {
+                        name: "Sushi",
+                        id: "sushi",
+                    },
+                    {
+                        name: "Pizza",
+                        id: "pizza",
+                    }
+                ]}
+            />
+        </div>
     ) : (
-            <div>
-                <h1>
-                    Please Log in
-                </h1>
-                <RegisterForm />
-            </div>
+        <div>
+            <h1>
+                Please Log in
+            </h1>
+            <RegisterForm/>
+        </div>
     );
 }
